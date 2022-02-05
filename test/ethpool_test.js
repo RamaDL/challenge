a2575c95baceef7916db0a0a0e1aa0a46d43c889
rluquez <dolz@dolz-2.local>
2022-02-05 19:08:45 -0300

    ADD test ethpool initial state

--- test/ethpool_test.js
+++ test/ethpool_test.js
@@ -1,13 +1,23 @@
-const ETHPool = artifacts.require("ETHPool");
+const ETHPoolContract = artifacts.require("ETHPool");
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("ETHPool", function (/* accounts */) {
-  it("should assert true", async function () {
-    await ETHPool.deployed();
+contract("ETHPoolContract deploy", accounts => {
+  it("should assert true", async () => {
+    await ETHPoolContract.deployed();
     return assert.isTrue(true);
+  });
+
+});
+
+describe("Initial ETHPool contract state", () => {
+  it("Should have 'name' specified at the constructor ", async () => {
+    const ethpool = await ETHPoolContract.deployed();
+    const expected = "ETH Pool";
+    const actual = await ethpool.name();
+    assert.equal(actual, expected, "ETH Pool");
   });
 });
