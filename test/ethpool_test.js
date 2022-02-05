0b193b903d8f086edf17790755831952e8b34c34
rluquez <dolz@dolz-2.local>
2022-02-05 17:14:26 -0300

    ADD ethpool test

---
+++ test/ethpool_test.js
@@ -0,0 +1,13 @@
+const ETHPoll = artifacts.require("ETHPool");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+// contract("ethpool_test", function (/* accounts */) {
+//   it("should assert true", async function () {
+//     await ethpool_test.deployed();
+//     return assert.isTrue(true);
+//   });
+// });
