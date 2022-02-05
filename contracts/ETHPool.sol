43028f53f2706d61604acc9f4cf95a9823819feb
rluquez <dolz@dolz-2.local>
2022-02-05 19:18:14 -0300

    ADD name to contract.

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -2,6 +2,14 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ETHPool {
+
+  // Contract name
+  string private _name = "ETH Pool"; 
+
   constructor() {
+  }
+
+  function name() public view virtual returns (string memory) {
+        return _name;
   }
 }
