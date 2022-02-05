c981ee0bf3c03c6832299589494678dc5121d833
rluquez <dolz@dolz-2.local>
2022-02-05 20:53:43 -0300

    ADD owner state to Ownable.

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "./Ownable.sol";
 
-contract ETHPool {
+contract ETHPool is Ownable{
 
   // Contract name
   string public name; 
