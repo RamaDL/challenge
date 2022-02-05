006e22d38786baa8193d8c31a8c52dc41d16c24e
rluquez <dolz@dolz-2.local>
2022-02-05 19:52:22 -0300

    ADD depositFunds method

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -4,8 +4,11 @@ pragma solidity >=0.4.22 <0.9.0;
 contract ETHPool {
 
   // Contract name
-  string public name = "ETH Pool"; 
+  string public name; 
 
   constructor() {
+    name = "ETH Pool";
   }
+
+  function deposit() public {}
 }
